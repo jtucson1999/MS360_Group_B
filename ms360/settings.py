dd05d0a9ef4f5fbbb8071ce52e0e6a4136a2b7af
jtucson1999 <jtucson1999@kaist.ac.kr>
2020-06-21 02:14:47 +0900

    hoxy

--- ms360/settings.py
+++ ms360/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['enigmatic-wave-25244.herokuapp.com']
 
 
 # Application definition
